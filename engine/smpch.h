62ada9b69f36579cdf0a78a29cbcfc3ee4ce4390
Vinicius de Oliveira <viniciusxyz@protonmail.com>
2022-03-20 16:01:27 -0300

    [Engine] and, of course, don't forget to keep track of the pch

---
+++ engine/smpch.h
@@ -0,0 +1,15 @@
+#ifndef SM_PCH_H
+#define SM_PCH_H
+
+#include <assert.h>
+#include <errno.h>
+#include <err.h>
+#include <math.h>    // mod sqrt sin cos...
+#include <stdbool.h> // bool true false
+#include <stddef.h>  // NULL
+#include <stdint.h>
+#include <stdio.h>  // printf
+#include <stdlib.h> // malloc calloc realloc free
+#include <string.h> // memcpy and string manipulation
+
+#endif /* SM_PCH_H */
