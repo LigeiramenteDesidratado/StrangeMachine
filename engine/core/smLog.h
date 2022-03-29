690294ac978d5fa5e2f7e06be3b711da643776d1
Vinicius de Oliveira <viniciusxyz@protonmail.com>
2022-03-28 22:34:15 -0300

    [Engine] log the module name

--- engine/core/smLog.h
+++ engine/core/smLog.h
@@ -4,21 +4,25 @@
 #ifdef SM_DEBUG
   #include "vendor/logc/src/log.h"
 
+  #ifndef SM_MODULE_NAME
+    #define SM_MODULE_NAME "NO_NAME"
+  #endif
+
   #define SM_FILE (__builtin_strrchr("/"__BASE_FILE__, '/') + 1)
 
-  #define SM_LOG_TRACE(...) log_log(LOG_TRACE, SM_FILE, __LINE__, "[SM] " __VA_ARGS__)
-  #define SM_LOG_DEBUG(...) log_log(LOG_DEBUG, SM_FILE, __LINE__, "[SM] " __VA_ARGS__)
-  #define SM_LOG_INFO(...)  log_log(LOG_INFO, SM_FILE, __LINE__, "[SM] " __VA_ARGS__)
-  #define SM_LOG_WARN(...)  log_log(LOG_WARN, SM_FILE, __LINE__, "[SM] " __VA_ARGS__)
-  #define SM_LOG_ERROR(...) log_log(LOG_ERROR, SM_FILE, __LINE__, "[SM] " __VA_ARGS__)
-  #define SM_LOG_FATAL(...) log_log(LOG_FATAL, SM_FILE, __LINE__, "[SM] " __VA_ARGS__)
+  #define SM_LOG_TRACE(...) log_log(LOG_TRACE, SM_FILE, __LINE__, "[SM " SM_MODULE_NAME "] " __VA_ARGS__)
+  #define SM_LOG_DEBUG(...) log_log(LOG_DEBUG, SM_FILE, __LINE__, "[SM " SM_MODULE_NAME "] " __VA_ARGS__)
+  #define SM_LOG_INFO(...)  log_log(LOG_INFO, SM_FILE, __LINE__, "[SM " SM_MODULE_NAME "] " __VA_ARGS__)
+  #define SM_LOG_WARN(...)  log_log(LOG_WARN, SM_FILE, __LINE__, "[SM " SM_MODULE_NAME "] " __VA_ARGS__)
+  #define SM_LOG_ERROR(...) log_log(LOG_ERROR, SM_FILE, __LINE__, "[SM " SM_MODULE_NAME "] " __VA_ARGS__)
+  #define SM_LOG_FATAL(...) log_log(LOG_FATAL, SM_FILE, __LINE__, "[SM " SM_MODULE_NAME "] " __VA_ARGS__)
 
-  #define SM_CORE_LOG_TRACE(...) log_log(LOG_TRACE, SM_FILE, __LINE__, "[SM CORE] " __VA_ARGS__)
-  #define SM_CORE_LOG_DEBUG(...) log_log(LOG_DEBUG, SM_FILE, __LINE__, "[SM CORE] " __VA_ARGS__)
-  #define SM_CORE_LOG_INFO(...)  log_log(LOG_INFO, SM_FILE, __LINE__, "[SM CORE] " __VA_ARGS__)
-  #define SM_CORE_LOG_WARN(...)  log_log(LOG_WARN, SM_FILE, __LINE__, "[SM CORE] " __VA_ARGS__)
-  #define SM_CORE_LOG_ERROR(...) log_log(LOG_ERROR, SM_FILE, __LINE__, "[SM CORE] " __VA_ARGS__)
-  #define SM_CORE_LOG_FATAL(...) log_log(LOG_FATAL, SM_FILE, __LINE__, "[SM CORE] " __VA_ARGS__)
+  #define SM_CORE_LOG_TRACE(...) log_log(LOG_TRACE, SM_FILE, __LINE__, "[SMC " SM_MODULE_NAME "] " __VA_ARGS__)
+  #define SM_CORE_LOG_DEBUG(...) log_log(LOG_DEBUG, SM_FILE, __LINE__, "[SMC " SM_MODULE_NAME "] " __VA_ARGS__)
+  #define SM_CORE_LOG_INFO(...)  log_log(LOG_INFO, SM_FILE, __LINE__,  "[SMC " SM_MODULE_NAME "] " __VA_ARGS__)
+  #define SM_CORE_LOG_WARN(...)  log_log(LOG_WARN, SM_FILE, __LINE__,  "[SMC " SM_MODULE_NAME "] " __VA_ARGS__)
+  #define SM_CORE_LOG_ERROR(...) log_log(LOG_ERROR, SM_FILE, __LINE__, "[SMC " SM_MODULE_NAME "] " __VA_ARGS__)
+  #define SM_CORE_LOG_FATAL(...) log_log(LOG_FATAL, SM_FILE, __LINE__, "[SMC " SM_MODULE_NAME "] " __VA_ARGS__)
 
 #else
 
