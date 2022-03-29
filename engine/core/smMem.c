3e10acd40ca0449682ba60bc8f3f52e9f0f8b17d
Vinicius de Oliveira <viniciusxyz@protonmail.com>
2022-03-28 22:33:28 -0300

    [Engine] functions renamed

--- engine/core/smMem.c
+++ engine/core/smMem.c
@@ -17,8 +17,8 @@ static struct {
 
 #define SM_MEM_HEADER_SIZE sizeof(size_t)
 
-static char *__smmem_human_readable_size(void);
-void *__smmem_malloc(size_t size) {
+static char *sm__mem_human_readable_size(void);
+void *sm__mem_malloc(size_t size) {
 
   /*
    * ISO/IEC 9899:TC3 section 6.5.3.4 The sizeof operator
@@ -47,7 +47,7 @@ void *__smmem_malloc(size_t size) {
   return ptr + SM_MEM_HEADER_SIZE;
 }
 
-void *__smmem_calloc(size_t nmemb, size_t size) {
+void *sm__mem_calloc(size_t nmemb, size_t size) {
 
   size_t _size = nmemb * size;
 
@@ -67,10 +67,10 @@ void *__smmem_calloc(size_t nmemb, size_t size) {
   return ptr + SM_MEM_HEADER_SIZE;
 }
 
-void *__smmem_realloc(void *ptr, size_t size) {
+void *sm__mem_realloc(void *ptr, size_t size) {
 
   if (!ptr)
-    return __smmem_malloc(size);
+    return sm__mem_malloc(size);
 
   char *newp = NULL;
   char *_ptr = (((char *)ptr) - SM_MEM_HEADER_SIZE);
@@ -90,7 +90,7 @@ void *__smmem_realloc(void *ptr, size_t size) {
   return _ptr + SM_MEM_HEADER_SIZE;
 }
 
-void __smmem_free(void *ptr) {
+void sm__mem_free(void *ptr) {
 
   if (ptr) {
 
@@ -107,7 +107,7 @@ void __smmem_free(void *ptr) {
 
 /* Alinged memory allocation */
 
-void *__smmem_aligned_alloc(size_t alignment, size_t size) {
+void *sm__mem_aligned_alloc(size_t alignment, size_t size) {
 
   void *ptr = NULL;
 
@@ -148,7 +148,7 @@ void *__smmem_aligned_alloc(size_t alignment, size_t size) {
   return ptr;
 }
 
-void __smmem_aligned_free(void *ptr) {
+void sm__mem_aligned_free(void *ptr) {
 
   if (ptr) {
 
@@ -158,6 +158,12 @@ void __smmem_aligned_free(void *ptr) {
     /* get the original address by subtracting the offset plus the header size from the aligned address */
     void *_ptr = ((char *)ptr) - (offset + SM_MEM_HEADER_SIZE);
 
+    size_t size = *((size_t *)_ptr);
+
+    mem_info.bytes -= size;
+    mem_info.allocs--;
+    mem_info.frees++;
+
     free(_ptr);
   }
 }
@@ -166,23 +172,24 @@ void __smmem_aligned_free(void *ptr) {
 #define BYTES2MB ((float)(1 << 20))
 #define BYTES2GB ((float)(1 << 30))
 
-void __smmem_print(void) {
-  printf("allocations: %lu\ntotal (re)alloctions: %lu\nbytes: %lu\ntotal bytes: %s\nfree calls: %lu\n", mem_info.allocs,
-         mem_info.total_allocs, mem_info.bytes, __smmem_human_readable_size(), mem_info.frees);
+void sm__mem_print(void) {
+  printf("allocations: %lu\ntotal (re)alloctions: %lu\nbytes: %lu %s\ntotal bytes: %s\nfree calls: %lu\n",
+         mem_info.allocs, mem_info.total_allocs, mem_info.bytes, (mem_info.bytes == 0) ? "" : "(Waring: memory leaks!)",
+         sm__mem_human_readable_size(), mem_info.frees);
 }
 
 // return a string with human readable memory size
-static char *__smmem_human_readable_size(void) {
+static char *sm__mem_human_readable_size(void) {
   static char buf[32];
   float size = (float)mem_info.total_bytes;
   if (size < BYTES2KB)
-    sprintf(buf, "%.3fB", size);
+    sprintf(buf, "%.2f B", size);
   else if (size < BYTES2MB)
-    sprintf(buf, "%.3fKB", size / BYTES2KB);
+    sprintf(buf, "%.2f KB", size / BYTES2KB);
   else if (size < BYTES2GB)
-    sprintf(buf, "%.3fMB", size / BYTES2MB);
+    sprintf(buf, "%.2f MB", size / BYTES2MB);
   else
-    sprintf(buf, "%.3fGB", size / BYTES2GB);
+    sprintf(buf, "%.2f GB", size / BYTES2GB);
   return buf;
 }
 
--- engine/core/smMem.h
+++ engine/core/smMem.h
@@ -5,24 +5,24 @@
 
 #ifdef SM_DEBUG
 
-void *__smmem_malloc(size_t size);
-void *__smmem_calloc(size_t nmemb, size_t size);
-void *__smmem_realloc(void *ptr, size_t size);
-void __smmem_free(void *ptr);
-void __smmem_print(void);
+void *sm__mem_malloc(size_t size);
+void *sm__mem_calloc(size_t nmemb, size_t size);
+void *sm__mem_realloc(void *ptr, size_t size);
+void sm__mem_free(void *ptr);
+void sm__mem_print(void);
 
-void *__smmem_aligned_alloc(size_t alignment, size_t size);
-void __smmem_aligned_free(void *ptr);
+void *sm__mem_aligned_alloc(size_t alignment, size_t size);
+void sm__mem_aligned_free(void *ptr);
 
   /* Memory */
-  #define SM_MALLOC(SIZE)        __smmem_malloc(SIZE)
-  #define SM_CALLOC(NMEMB, SIZE) __smmem_calloc(NMEMB, SIZE)
-  #define SM_REALLOC(PTR, SIZE)  __smmem_realloc(PTR, SIZE)
-  #define SM_FREE(PTR)           __smmem_free(PTR)
+  #define SM_MALLOC(SIZE)        sm__mem_malloc(SIZE)
+  #define SM_CALLOC(NMEMB, SIZE) sm__mem_calloc(NMEMB, SIZE)
+  #define SM_REALLOC(PTR, SIZE)  sm__mem_realloc(PTR, SIZE)
+  #define SM_FREE(PTR)           sm__mem_free(PTR)
 
   /* Aligned memory*/
-  #define SM_ALIGNED_ALLOC(ALIGNMENT, SIZE) __smmem_aligned_alloc(ALIGNMENT, SIZE)
-  #define SM_ALIGNED_FREE(PTR)              __smmem_aligned_free(PTR)
+  #define SM_ALIGNED_ALLOC(ALIGNMENT, SIZE) sm__mem_aligned_alloc(ALIGNMENT, SIZE)
+  #define SM_ALIGNED_FREE(PTR)              sm__mem_aligned_free(PTR)
 
 #else
 
@@ -35,7 +35,7 @@ void __smmem_aligned_free(void *ptr);
   #define SM_ALIGNED_ALLOC(ALIGNMENT, SIZE) aligned_alloc(ALIGNMENT, SIZE)
   #define SM_ALIGNED_FREE(PTR)              free(PTR)
 
-void __smmem_print(void);
+void sm__mem_print(void);
 
 #endif /* SM_DEBUG */
 
