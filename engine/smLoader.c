800a93a58cbea53e7cda9787285194dcf26ecc9a
Vinicius de Oliveira <viniciusxyz@protonmail.com>
2022-04-11 21:49:29 -0300

    [Engine] stb_img macros

--- engine/smLoader.c
+++ engine/smLoader.c
@@ -1,7 +1,14 @@
 #include "smpch.h"
 
+#include "core/smAssert.h"
+#include "core/smMem.h"
+
+#define STBI_MALLOC(sz)        SM_MALLOC(sz)
+#define STBI_REALLOC(p, newsz) SM_REALLOC(p, newsz)
+#define STBI_FREE(p)           SM_FREE(p)
+#define STBI_ASSERT(x)         SM_ASSERT(x)
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb/stb_image.h"
+#include "vendor/stbi/stb_image.h"
 
 #define CGLTF_IMPLEMENTATION
 #include "cgltf/cgltf.h"
