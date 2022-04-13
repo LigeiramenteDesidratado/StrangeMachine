fa4fbc54769cde600f1338b30a9c64669b6b719c
Vinicius de Oliveira <viniciusxyz@protonmail.com>
2022-04-13 02:18:16 -0300

    [Engine] include

--- engine/core/smCore.h
+++ engine/core/smCore.h
@@ -1,17 +1,22 @@
 #ifndef SM_CORE_H
 #define SM_CORE_H
 
-#include "core/smWindow.h"
 #include "core/smApplication.h"
 #include "core/smAssert.h"
 #include "core/smBase.h"
 #include "core/smLog.h"
+#include "core/smWindow.h"
 
 #include "core/smKeyCode.h"
 #include "core/smMouseCode.h"
 
+#include "core/smHandle.h"
+
 #include "core/util/smUtil.h"
 
 #include "core/smMem.h"
+
+#include "core/thread/smThread.h"
+#include "core/thread/synchronization/smMutex.h"
 
 #endif /* SM_CORE_H */
